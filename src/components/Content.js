0f938d491d765a53255f717356a186157255e214
guti <luchoguti9@gmail.com>
2017-11-27 10:41:22 -0500

    Se crear primer proyecto react js

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <h1>este es el contenido</h1>
+        <p>lorem implements...........</p>
+      </div>
+    );
+  }
+}
+
+export default Content;

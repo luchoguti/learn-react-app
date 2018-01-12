06d1ab5e3946796cd807964a39200aadf8f4ca7e
guti9 <luchoguti9@gmail.com>
2018-01-12 18:06:32 -0500

    lesson 6

---
+++ src/server/index.js
@@ -0,0 +1,36 @@
+// Dependecies
+
+import express from "express";
+import webpack from "webpack";
+import path from "path";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotDevMiddleware from "webpack-hot-middleware";
+import open from "open";
+
+//Webpack Configuration
+import webpackConfig from '../../webpack.config.dev';
+
+// Server port
+const port = 3000;
+
+// Express app
+const app = express();
+
+// Webpack Compiler
+const webpackCompiler = webpack(webpackConfig);
+
+// Webpack Middlwares
+app.use(webpackDevMiddleware(webpackCompiler));
+app.use(webpackHotMiddleware(webpackCompiler));
+
+//Sending all traffic to React
+app.get('*',(req,res)=>{
+	res.sendFile(path.join(__dirname,"../public/index.html"));
+});
+
+// Listen port
+app.listen(port, err =>{
+	if (!err) {
+		open(`http://localhost:${port}`);
+	}
+});
